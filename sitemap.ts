271ca64897fa266f676e39dcb15b345bf1cb7797
mercadofavoravel-afk <mercadofavoravel@gmail.com>
2026-07-28 15:21:55 -0300

    Add files via upload

---
+++ sitemap.ts
@@ -0,0 +1,1 @@
+import type {MetadataRoute} from "next";import {projects} from "@/lib/projects";export default function sitemap():MetadataRoute.Sitemap{const b="https://www.imoveisdealtopadraorio.com.br";return [{url:b,lastModified:new Date()},...projects.map(p=>({url:`${b}/empreendimentos/${p.slug}`,lastModified:new Date()}))]}

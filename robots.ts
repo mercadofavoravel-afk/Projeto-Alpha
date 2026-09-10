9fd836dfcc5729b2c638cb59e4830ad875645f20
mercadofavoravel-afk <mercadofavoravel@gmail.com>
2026-09-10 16:03:15 -0300

    Usar domínio oficial centralizado no robots

--- robots.ts
+++ robots.ts
@@ -1,7 +1,9 @@
 import type { MetadataRoute } from 'next';
+import { siteUrl } from '@/lib/site-config';
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [{ userAgent: '*', allow: '/', disallow: ['/admin'] }],
-    sitemap: 'https://www.imoveisdealtopadraorio.com.br/sitemap.xml',
+    sitemap: siteUrl('/sitemap.xml'),
   };
 }
